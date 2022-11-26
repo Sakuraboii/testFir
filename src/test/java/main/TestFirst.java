8c1fe32dd515bed7b357374a0603b3c5bd1af98b
Sakuraboii <89277584951@mail.ru>
2022-11-26 20:25:35 +0400

    Может получиться6

--- src/test/java/main/TestFirst.java
+++ src/test/java/main/TestFirst.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class TestFirst {
 
     @Test
-    public void main(String[] args){
+    public void main(){
         System.out.println("hello test");
         int users = 2;
     }
